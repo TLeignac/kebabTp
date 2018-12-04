eec21b7eff841146f07e0c33b1b6ff1411dc8737
TLeignac <thomas.leignac@outlook.fr>
2018-12-04 08:49:58 +0100

    decorator 2régimes 10 ingrédients 1 assiette

---
+++ kebabvisitor/src/main/java/fr/unilim/iut/kebabvisitor/Kebab.java
@@ -0,0 +1,10 @@
+package fr.unilim.iut.kebabvisitor;
+
+import java.util.List;
+import fr.unilim.iut.kebabvisitor.ingredients.Ingredient;
+
+public interface Kebab {
+	List<Ingredient> getIngredients();
+	boolean isVegetarien();
+	boolean isPescetarien();
+}
